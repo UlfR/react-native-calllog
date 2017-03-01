e805b5c87298692d1b175a77550836c2c27e9b4f
Gleb Stiblo <errandir@gmail.com>
2017-03-01 17:51:46 +0600

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //FIXME сделать
+import { NativeModules } from 'react-native';
+
+export default const CallLog = {
+    getList: (params, callback) => {
+        //FIXME params
+        return NativeModules.CallLog.getCallLog(callback);
+    }
+};
